e89ad714fb177184a23ea30a63d92b3833f1e5b0
Alicia Lim <alicialim@users.noreply.github.com>
2019-07-05 00:16:47 +0700

    Added skeleton structure for project

---
+++ src/test/java/anz/assessment/AnzAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package anz.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnzAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
